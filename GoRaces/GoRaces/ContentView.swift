7e67df576e0dcceb6a3e119bfd51d5e156409502
igorelik <gorelik.igor@gmail.com>
2024-07-19 17:31:21 +1000

    Initial push

---
+++ GoRaces/GoRaces/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoRaces
+//
+//  Created by Igor Gorelik on 19/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
