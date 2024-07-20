18423c8867cfbeda36f78625b4df3cb91a95b72e
igorelik <gorelik.igor@gmail.com>
2024-07-20 18:46:52 +1000

    Race timers

--- GoRaces/GoRaces/Models/RaceModel.swift
+++ GoRaces/GoRaces/Models/RaceModel.swift
@@ -9,6 +9,14 @@ public struct RaceModel {
     let meetingName: String
     let advertisedStartTime: Date
     
+    func intervalTill( time: Date) -> String {
+        let secondsInterval = advertisedStartTime.timeIntervalSince1970 - time.timeIntervalSince1970
+        let raceNotStarted = secondsInterval > 0
+        let minutes = Int((secondsInterval.magnitude / 60).rounded(.down))
+        let seconds = Int(secondsInterval.magnitude.truncatingRemainder(dividingBy: 60).magnitude)
+        return "\(raceNotStarted ? "" : "-")\(String(format: "%02d", minutes)):\(String(format: "%02d", seconds))"
+    }
+    
     init(raceID: String, raceName: String, raceNumber: Int, raceType: RaceType, meetingName: String, advertisedStartTime: Date) {
         self.raceID = raceID
         self.raceName = raceName
--- GoRaces/GoRaces/Services/Protocols/RaceQueryServiceProtocol.swift
+++ GoRaces/GoRaces/Services/Protocols/RaceQueryServiceProtocol.swift
@@ -2,4 +2,5 @@ import Foundation
 
 public protocol RaceQueryServiceProtocol {
     func getRaces(for raceTypes: [RaceType], max: Int) async throws -> [RaceModel]
+    func filterRaces(races: [RaceModel], raceTypes: [RaceType], max: Int) -> [RaceModel] 
 }
