7ddc6ef3f0cc28749add4d866935998223e9cc5d
igorelik <gorelik.igor@gmail.com>
2024-07-21 10:40:14 +1000

    Filtering implementation

--- GoRaces/GoRaces/Services/RaceQueryService.swift
+++ GoRaces/GoRaces/Services/RaceQueryService.swift
@@ -23,10 +23,13 @@ public class RaceQueryService: RaceQueryServiceProtocol {
     
     public func filterRaces(races: [RaceModel], raceTypes: [RaceType], max: Int) -> [RaceModel] {
         let relevantRaces = races.filter{ $0.raceType != .other }
+            .sorted(by: { r1, r2 in
+                r1.advertisedStartTime < r2.advertisedStartTime
+            })
             .filter { $0.advertisedStartTime.timeIntervalSince(Date.now) > -60 }
-        if raceTypes.isEmpty {
+        if raceTypes.isEmpty || raceTypes.count == RaceType.allCases.count - 1 {
             // return all relevant races
-            return relevantRaces
+            return relevantRaces.prefix(max > 0 ? max : relevantRaces.count).filter { _ in true }
         }
         // filter by selected race types
         return relevantRaces.filter { race in
@@ -34,11 +37,12 @@ public class RaceQueryService: RaceQueryServiceProtocol {
                 rt == race.raceType
             })
         }
+        .prefix(max).filter { _ in true }
     }
     
     public func getRaces(for raceTypes: [RaceType], max: Int) async throws -> [RaceModel] {
         // TODO: refactor to use config to build URL
-        let url = "https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=\(max*RaceType.allCases.count*3)"
+        let url = "https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=\(15*RaceType.allCases.count)"
         let data = try await fetchData(RacesDTO.self, for: URLRequest(url: URL(string: url)!))
         return filterRaces(races: data.data.raceSummaries.values
             .compactMap{ RaceModel(raceSummary: $0) }, raceTypes: raceTypes, max: max)
